5576ebcb0a3bb5d8c2fce4a69a32a95c8ff7438b
Wolfgang Kleinbach <register@zeichensatz.de>
2023-04-19 17:33:27 +0200

    Changes for TYPO 12 and Update to Extensionbuilder 11.0.9

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3') || die();
+defined('TYPO3') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('wok_photostation', 'Configuration/TypoScript', 'PhotoStation');
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3') || die();
+defined('TYPO3') or die('Access denied.');
 
 (static function() {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
