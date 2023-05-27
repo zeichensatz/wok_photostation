b84ea779aa5e5dc9db58860f00c5c5f318979849
Wolfgang Kleinbach <register@zeichensatz.de>
2023-05-27 14:06:45 +0200

    Removed language references for options: Not working in TYPO3 12 anymore 	Changed:       Configuration/TypoScript/constants.ts

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -29,24 +29,29 @@ plugin.tx_wokphotostation_albumdisplay {
 		jsSourceFile=EXT:wok_photostation/Resources/Public/PhotoStation/jsSource/syno_photo_external.js
 		# cat=PhotoStation/1000/20; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.resources.cssFile.label
 		cssFile=EXT:wok_photostation/Resources/Public/Scss/Theme/theme.scss
-
 		# cat=PhotoStation/1010/30; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.server.label
 		server=your.synology.com
 		# cat=PhotoStation/1010/40; type=options[https://=https://,http://=http://]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.http.label
 		http=https://
 		# cat=PhotoStation/1010/50; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.pageString.label
 		pageString=/photo/embed/embed.html?album=
-		# cat=PhotoStation/1100/10; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.autoplay.label
+//		# cat=PhotoStation/1100/10; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.autoplay.label
+		# cat=PhotoStation/1100/10; type=options[true=true,false=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.autoplay.label
 		autoplay=true
-		# cat=PhotoStation/1100/20; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.openps.label
+//		# cat=PhotoStation/1100/20; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.openps.label
+		# cat=PhotoStation/1100/20; type=options[true=true,false=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.openps.label
 		openps=true
-		# cat=PhotoStation/1100/30; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.lightbox.label
+//		# cat=PhotoStation/1100/30; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.lightbox.label
+		# cat=PhotoStation/1100/30; type=options[true=true,false=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.lightbox.label
 		lightbox=true
-		# cat=PhotoStation/1100/40; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.showDescription.label
+//		# cat=PhotoStation/1100/40; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.showDescription.label
+		# cat=PhotoStation/1100/40; type=options[true=true,false=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.showDescription.label
 		showDescription=true
-		# cat=PhotoStation/1100/50; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.debug.label
+//		# cat=PhotoStation/1100/50; type=options[LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.on.label=true,LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.off.label=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.debug.label
+		# cat=PhotoStation/1100/50; type=options[true=true,false=false]; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.generalSettings.debug.label
 		debug=false
 	}
+
 	view {
 		# cat=PhotoStation/1300/templaterootpath; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.templates.templateRootPath.label
 		templateRootPath = EXT:wok_photostation/Resources/Private/Templates/
@@ -55,10 +60,5 @@ plugin.tx_wokphotostation_albumdisplay {
 		# cat=PhotoStation/1300/layoutrootpath; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.templates.layoutRootPath.label
 		layoutRootPath = EXT:wok_photostation/Resources/Private/Layouts/
 	}
-/*
-	persistence {
-		# cat=PhotoStation/1400/storagepid; type=string; label=LLL:EXT:wok_photostation/Resources/Private/Language/locallang.xlf:constants.storage.storagePid.label
-		storagePid =
-	}
-*/
+
 }
